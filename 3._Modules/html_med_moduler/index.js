49189379a6ef3d5117b4e5aad34be8bb337cbc93
Rasmus <rasmusorum@gmail.com>
2024-02-08 09:01:33 +0100

    hej

---
+++ 3._Modules/html_med_moduler/index.js
@@ -0,0 +1,5 @@
+﻿export default class ClassA {
+    constructor() {
+        console.log("Class A instantiated")
+    }
+}
--- File_formats_Bonanza/parser1.cs
+++ File_formats_Bonanza/parser1.cs
@@ -1,7 +1,33 @@
-class Program
-{
-    static void Main()
-    {
+using System;
+using System.IO;
 
-    }
+namespace parser1
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader("file1.txt"))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }    
 }
